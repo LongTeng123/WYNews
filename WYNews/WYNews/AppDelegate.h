d70b47c1a5b97322389db92962150f9e207cdebb
隆腾 <574051825@qq.com>
2016-08-18 17:32:39 +0800

    第一次提交版本

---
+++ WYNews/WYNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYNews
+//
+//  Created by long on 16/8/18.
+//  Copyright © 2016年 long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WYNews/WYNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WYNews
+//
+//  Created by long on 16/8/18.
+//  Copyright © 2016年 long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
